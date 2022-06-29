642ecdb0f2533a7f8f8e0c77f28d69419e77a472
James Hill <jahill@groupon.com>
2022-06-29 13:39:11 +0100

    chore: adjusted hedron scene file

--- src/scene/spinningHedron.scene.ts
+++ src/scene/spinningHedron.scene.ts
@@ -2,14 +2,27 @@ import { Mesh, IcosahedronGeometry, MeshBasicMaterial } from 'three';
 
 /* -----------------------------------
  *
+ * Geometry
+ *
+ * -------------------------------- */
+
+const hedronGeometry = new IcosahedronGeometry(2, 1);
+
+/* -----------------------------------
+ *
+ * Material
+ *
+ * -------------------------------- */
+
+const hedronMaterial = new MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
+
+/* -----------------------------------
+ *
  * Mesh
  *
  * -------------------------------- */
 
-const spinningHedron = new Mesh(
-  new IcosahedronGeometry(1, 1),
-  new MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
-);
+const spinningHedron = new Mesh(hedronGeometry, hedronMaterial);
 
 /* -----------------------------------
  *
